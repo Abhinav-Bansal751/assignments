4483ff828aa8640f8e26dc1d6481941318320ee0
badboi-s <bansalabhinav751@gmail.com>
2024-10-21 20:59:02 +0530

    mongodb assignment

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -6,6 +6,8 @@ mongoose.connect('your-mongodb-url');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:{type:String},
+    password:{type:String}
 });
 
 const UserSchema = new mongoose.Schema({
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,10 +1,27 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement admin signup logic
+    const username = req.header;
+    const password = req.header;
+
+    try {
+        // check if a user with this username already exists
+        const existingAdmin  = await Admin.findOne({username});
+        if( existingAdmin  ) return res.status(200).send("user with that username already exists");
+
+        await Admin.create({username:username,password:password});
+        res.status(200).json({ message: 'Admin created successfully' });
+        
+    } catch (error) {
+        console.log("error occured in creating admin ",error);
+        res.status(500).send("error occured ");
+    }
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
